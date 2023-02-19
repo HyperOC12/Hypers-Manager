1bd1809dc6b5f5b483ab3d071785392a1b0820e1
Sangeet Parashar <parasharsangeet976@outlook.com>
2023-02-19 15:27:02 +0530

    Added tictactoe back and fixed bugs and also modified CONTRIBUTING.MD

---
+++ src/commands/games/tictactoe.js
@@ -0,0 +1,25 @@
+const { ChatInputCommandInteraction, SlashCommandBuilder } = require('discord.js');
+const { Game } = require('./builder')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('tictactoe')
+        .setDescription('Starts a new tictactoe game.')
+        .addUserOption(option => option
+            .setName('user')
+            .setDescription('The user to play with.')
+            .setRequired(true)
+        ),
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction, client) {
+        const { options, user } = interaction;
+
+        const Target = options.getUser('user');
+        if (Target.bot) return await interaction.reply({ content: 'Cannot play against bots.', ephemeral: true });
+
+        const game = new Game(Target, user, interaction);
+        await game.run();
+    },
+};
--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -3,39 +3,39 @@ const { Default_Embed_Colour } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Brings up the help menu.'),
+        .setName('help')
+        .setDescription('Brings up the help menu.'),
     /**
      * @param {ChatInputCommandInteraction} interaction
      * @param {Client} client
      */
     async execute(interaction, client) {
         const HelpEmbed = new EmbedBuilder()
-        .setColor(Default_Embed_Colour)
-        .setTitle('Help')
-        .setDescription(`${client.user.username} is a private moderation and utility bot for JayCord, currently managed and maintaned by the lovely development team!`)
-        .setFields(
-            {
-                name: '• Info',
-                value: inlineCode('help, membercount, serverinfo, userinfo')
-            },
-            {
-                name: '• Moderation',
-                value: inlineCode('ban, purge, kick, lock, unlock, mod, mute, unmute, nick, slowmode, unban, warn, rmpunish')
-            },
-            {
-                name: '• Misc',
-                value: inlineCode('avatar, freeban, poll, say')
-            },
-            {
-                name: '• Games',
-                value: inlineCode('8ball')
-            },
-            {
-                name: '• Util',
-                value: inlineCode('automod, customrole, debug, ping, status, blacklist')
-            }
-        )
+            .setColor(Default_Embed_Colour)
+            .setTitle('Help')
+            .setDescription(`${client.user.username} is a private moderation and utility bot for JayCord, currently managed and maintaned by the lovely development team!`)
+            .setFields(
+                {
+                    name: '• Info',
+                    value: inlineCode('help, membercount, serverinfo, userinfo')
+                },
+                {
+                    name: '• Moderation',
+                    value: inlineCode('ban, purge, kick, lock, unlock, mod, mute, unmute, nick, slowmode, unban, warn, rmpunish')
+                },
+                {
+                    name: '• Misc',
+                    value: inlineCode('avatar, freeban, poll, say')
+                },
+                {
+                    name: '• Games',
+                    value: inlineCode('tictactoe, 8ball')
+                },
+                {
+                    name: '• Util',
+                    value: inlineCode('automod, customrole, debug, ping, status, blacklist')
+                }
+            )
 
         interaction.reply({ embeds: [HelpEmbed] });
     },
