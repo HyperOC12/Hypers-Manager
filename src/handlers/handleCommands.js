9ea38b64720841c551c0f7f46fcfd89e2ac8da58
Jay <jayedmead2456@icloud.com>
2023-02-26 20:12:34 +0000

    convert to global commands

--- src/handlers/handleCommands.js
+++ src/handlers/handleCommands.js
@@ -18,7 +18,7 @@ module.exports = (client) => {
         const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
         try {
-            await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: client.commandsArray });
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: client.commandsArray });
             console.log('[Commands]: Loaded');
         } catch (error) {
             console.log(error);
